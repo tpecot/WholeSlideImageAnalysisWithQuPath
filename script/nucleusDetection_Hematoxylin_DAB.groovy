2e821251bd12eb1d1b05ca69906ab1333ca34b54
tpecot <36488353+tpecot@users.noreply.github.com>
2022-03-28 15:30:48 +0200

    Add files via upload

---
+++ script/nucleusDetection_Hematoxylin_DAB.groovy
@@ -0,0 +1,37 @@
+import qupath.ext.stardist.StarDist2D
+import qupath.lib.images.servers.ColorTransforms
+import qupath.imagej.gui.ImageJMacroRunner
+
+min_nuclei_area = 15
+
+// Specify the model directory (you will need to change this!)
+def pathModel = "C:/Work/QuPath/scripts/StardistModels/TissueNet_all.pb"
+
+def stardist_segmentation = StarDist2D.builder(pathModel)
+        .threshold(0.5)              // Prediction threshold
+        .normalizePercentiles(1, 99.8) // Percentile normalization
+        .pixelSize(0.5)              // Resolution for detection
+        .channels(
+            ColorTransforms.createColorDeconvolvedChannel(getCurrentImageData().getColorDeconvolutionStains(), 1),
+            ColorTransforms.createColorDeconvolvedChannel(getCurrentImageData().getColorDeconvolutionStains(), 2)
+            )
+        .cellExpansion(5.0)          // Approximate cells based upon nucleus expansion
+        .cellConstrainScale(1.5)     // Constrain cell expansion using nucleus size
+        .measureShape()              // Add shape measurements
+        .measureIntensity()          // Add cell measurements (in all compartments)
+        .build()
+
+        
+def imageData = getCurrentImageData()
+def hierarchy = imageData.getHierarchy()
+def annotations = hierarchy.getAnnotationObjects()
+
+// Run detection for the selected objects
+stardist_segmentation.detectObjects(imageData, annotations)
+
+//def toDelete = getDetectionObjects().findAll {measurement(it, 'Circularity') < 0.9}
+def toDelete = getDetectionObjects().findAll {measurement(it, 'Area µm^2') < min_nuclei_area}
+removeObjects(toDelete, true)
+
+
+println 'Done!'
---
+++ script/nucleusDetection_fluo.groovy
@@ -0,0 +1,38 @@
+import qupath.ext.stardist.StarDist2D
+import qupath.lib.images.servers.ColorTransforms
+import qupath.imagej.gui.ImageJMacroRunner
+
+min_nuclei_area = 15
+
+// Specify the model directory (you will need to change this!)
+def pathInput = buildFilePath(PROJECT_BASE_DIR)
+def pathModel = pathInput + "/TissueNet_all.pb"
+
+def stardist_segmentation = StarDist2D.builder(pathModel)
+        .threshold(0.5)              // Prediction threshold
+        .normalizePercentiles(1, 99.8) // Percentile normalization
+        .pixelSize(1)              // Resolution for detection
+        .channels(0)
+        .cellExpansion(5.0)          // Approximate cells based upon nucleus expansion
+        .cellConstrainScale(1.5)     // Constrain cell expansion using nucleus size
+        .measureShape()              // Add shape measurements
+        .measureIntensity()          // Add cell measurements (in all compartments)
+        .build()
+
+        
+def imageData = getCurrentImageData()
+def pathObjects = getSelectedObjects()
+if (pathObjects.isEmpty()) {
+	Dialogs.showErrorMessage("StarDist", "Please select a parent object!")
+	return
+}
+
+// Run detection for the selected objects
+stardist_segmentation.detectObjects(imageData, pathObjects)
+
+//def toDelete = getDetectionObjects().findAll {measurement(it, 'Circularity') < 0.9}
+def toDelete = getDetectionObjects().findAll {measurement(it, 'Area µm^2') < min_nuclei_area}
+removeObjects(toDelete, true)
+
+
+println 'Done!'
