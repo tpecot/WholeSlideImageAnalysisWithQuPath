07470cd71243fe31529fbcb1ba8480e6de7b1ea8
tpecot <36488353+tpecot@users.noreply.github.com>
2023-01-31 11:08:51 +0100

    Add files via upload

--- script/nucleusDetection_Hematoxylin_DAB.groovy
+++ script/nucleusDetection_Hematoxylin_DAB.groovy
@@ -8,16 +8,20 @@ min_nuclei_area = 15
 def pathInput = buildFilePath(PROJECT_BASE_DIR)
 def pathModel = pathInput + "/script/stardist_model_1_channel.pb"
 
-def stardist_segmentation = StarDist2D.builder(pathModel)
+def stardist_segmentation = StarDist2D
+        .builder(pathModel)
+        .preprocess(                 // Apply normalization, calculating values across the whole image
+            StarDist2D.imageNormalizationBuilder()
+                .maxDimension(4096)    // Figure out how much to downsample large images to make sure the width & height are <= this value
+                .percentiles(1, 99.8)  // Calculate image percentiles to use for normalization
+                .build()
+        	)
         .threshold(0.5)              // Prediction threshold
-        .normalizePercentiles(1, 99.8) // Percentile normalization
         .pixelSize(0.5)              // Resolution for detection
         .channels(
             ColorTransforms.createColorDeconvolvedChannel(getCurrentImageData().getColorDeconvolutionStains(), 1),
             ColorTransforms.createColorDeconvolvedChannel(getCurrentImageData().getColorDeconvolutionStains(), 2)
             )
-        .cellExpansion(5.0)          // Approximate cells based upon nucleus expansion
-        .cellConstrainScale(1.5)     // Constrain cell expansion using nucleus size
         .measureShape()              // Add shape measurements
         .measureIntensity()          // Add cell measurements (in all compartments)
         .build()
@@ -30,7 +34,7 @@ def annotations = hierarchy.getAnnotationObjects()
 // Run detection for the selected objects
 stardist_segmentation.detectObjects(imageData, annotations)
 
-def toDelete = getDetectionObjects().findAll {measurement(it, 'Nucleus: Area µm^2') < min_nuclei_area}
+def toDelete = getDetectionObjects().findAll {measurement(it, 'Area µm^2') < min_nuclei_area}
 removeObjects(toDelete, true)
 
 
